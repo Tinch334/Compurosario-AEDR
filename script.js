0912311333cb33ef0c240645959b06d0cdd7a8ce
maph <juani.bertoni73@gmail.com>
2022-05-05 14:02:42 -0300

    new carousel

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-$(document).ready(function () {
-  $('.carousel').slick({
-    prevArrow:
-      '<button class="slick-prev" aria-label="Previous" type="button">&lt;</button>',
-    nextArrow:
-      '<button class="slick-next" aria-label="Next" type="button">&gt;</button>',
-
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4
-  });
+$(document).ready(function() {
+  $('#autoWidth').lightSlider({
+      autoWidth:true,
+      loop:true,
+      onSliderLoad: function() {
+          $('#autoWidth').removeClass('cS-hidden');
+      } 
+  });  
 });
