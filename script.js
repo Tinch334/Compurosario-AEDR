6ca76a51c347da5604c76c472caf2c73c13e0f40
Martin Goñi <46424341+Tinch334@users.noreply.github.com>
2022-06-12 03:26:13 -0300

    Tried new method for segmentation

--- script.js
+++ script.js
@@ -30,3 +30,12 @@ $(document).ready(function() {
     });
     
 });
+
+
+$(function () {
+  var includes = $('[data-include]')
+  $.each(includes, function () {
+    var file = 'templates/' + $(this).data('include') + '.html'
+    $(this).load(file)
+  })
+})
