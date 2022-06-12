bfc45c41e4e8d88cc2d14a689e9785facef2f430
Martin Goñi <46424341+Tinch334@users.noreply.github.com>
2022-06-12 17:47:53 -0300

    AJAX register, style improvements
    Register page now uses AJAX, it's style is now tolerable

--- UserHandling/register.php
+++ UserHandling/register.php
@@ -8,16 +8,17 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
+if (isset($_POST['username']) && $_POST['username'] && isset($_POST['email']) && $_POST['email'] && isset($_POST['password']) && $_POST['password']) {
+
     $checkRepUser = $mysqli->query("select * from users where username ='".$_POST['username']."'");
     $checkRepEmail = $mysqli->query("select * from users where email ='".$_POST['email']."'");
 
     //We check to make shure the email or username are not in use
     if ($checkRepEmail->num_rows) {
-        print ("mail repetido");
-        exit;
+        echo json_encode(array('success' => -2));
+
     } else if ($checkRepUser->num_rows) {
-        print ("user repetido");
-        exit;
+        echo json_encode(array('success' => -1));
     }
     else {
         // para verificar el login se usa password_verify($_POST['password'],$encryptedPass)
@@ -62,12 +63,58 @@ require 'PHPMailer/src/SMTP.php';
             //We add the user if the email was sent
             $mysqli->query("INSERT INTO users(username, email, email_verif_code, password) VALUES('" . $_POST['username'] . "', '" . $_POST['email'] . "', '" . $token . "', '" . $encryptedPass . "')");
 
-            //Take user to main page.
-            header("Location: /TRES/index.html");
+            //The user registration was successful.
+            echo json_encode(array('success' => 1));
         }
     }
-
+}
 
 $mysqli->close();
-
 ?>
+
+
+
+<?php
+/*include "data.php";
+session_start();
+
+    //We check that we got the username and password
+    if (isset($_POST['username']) && $_POST['username'] && isset($_POST['password']) && $_POST['password']) {
+        $query = $mysqli->query("select * from users where username ='".$_POST['username']."'");
+        $encryptedPass = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+        if ($query->num_rows) {
+            $row = $query->fetch_array(MYSQLI_ASSOC);
+
+            if ($row["account_verified"] == true) {
+                if (password_verify($_POST['password'], $row["password"])) {
+                    //We update the last time the user logged in.
+                    $date = date("Y-m-d H:i:s");
+                    $mysqli->query("UPDATE users set last_log = '" . $date . "' WHERE username= '" . $_POST['username'] . "'");
+
+                    //NOT WORKING
+                    //Cookies do not work in any of the computers of the members of the group.
+                    //We create a cookie that will expire in 30 days and grants the user access to all the files.
+                    //setcookie("userLogged", "pito de mono" , time()+60*60*24*30, "/", 1);
+
+                    //Using sessions until issue is resolved.
+                    $_SESSION["logged-in"] = $row["id"];
+
+                    echo json_encode(array('success' => 1)); //Successful verification.   
+                }
+                else {
+                    echo json_encode(array('success' => -3));
+                }
+            }
+            else {
+                echo json_encode(array('success' => -2));
+            }
+        }
+        else {
+            echo json_encode(array('success' => -1));
+        }    
+    }
+    else {
+        echo json_encode(array('success' => 0));
+    }
+?>*/
