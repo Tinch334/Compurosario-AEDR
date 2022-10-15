45c6211170c3dec91b017bdaa76bf0cbcc9814df
maph <juani.bertoni73@gmail.com>
2022-10-15 01:14:27 -0300

    admin system almost finished

--- admin/users.php
+++ admin/users.php
@@ -24,15 +24,18 @@ session_start();
 
     echo "<p>No olvides <a href='logout.php'>Cerrar sesion</a> al terminar :)</p>";
 
+    echo "<h2>Usuarios actuales</h2>";
+
+ 
     echo("<table>"."\n");
-        $stmt = $mysqli->query("SELECT username, email, account_verified, last_log from users");
+        $stmt = $mysqli->query("SELECT username, email, account_verified, num_of_logs, last_log from users where is_blocked=0");
 
 
         echo("<td><b>Nombre de usuario</b></td>");
         echo("<td><b>Correo electrónico</b</td>");
         echo("<td><b>Cuenta verificada</b</td>");
-        echo("<td><b>Ultimo inicio de sesión</b</td>");
         echo("<td><b>Numero de inicios de sesion</b</td>");
+        echo("<td><b>Ultimo inicio de sesión</b</td>");
         echo("<td><b>Administrar usuario</b</td>");
 
         while ( $row = $stmt->fetch_assoc() ) {
@@ -41,16 +44,16 @@ session_start();
             echo "</td><td>";
             echo(htmlentities($row['email']));
             echo "</td><td>";
-            echo(htmlentities($row['account_verified']));
+            echo(htmlentities($row['account_verified'])? "Sí" : "No");
             echo "</td><td>";
-            echo(htmlentities($row['last_log']));
+            echo(htmlentities($row['num_of_logs']));
             echo "</td><td>";
-            echo 0;
-            //echo(htmlentities($row['num_of_logs']));
+            echo(htmlentities($row['last_log'])? htmlentities($row['last_log']) : "Sin registro");
             echo "</td><td>";
             echo(
-            '<form action="block_user.php" method="post">
+            '<form action="manage_user.php" method="post">
             <input placeholder="Ingresar motivo de bloqueo" type="text" id="reason" name="reason" required="true">
+            <input type="hidden" name="todo" value=block>
             <input type="hidden" name="foo" value='.htmlentities($row["username"]).'>
             <input type="submit" value="Bloquear">
             </form>'
@@ -58,14 +61,58 @@ session_start();
             echo("\n"."</td></tr>"."\n");
         }
     ?>
-
-      
-
-        
-
-
     </table>
 
-    <a href='admin.php'>Volver</a>
+    <?php
+    echo "<h2>Usuarios bloqueados</h2>";
+
+    echo("<table>"."\n");
+    
+        $stmt2 = $mysqli->query("SELECT username, email, account_verified, num_of_logs, last_log from users where is_blocked=1");
+
+        if ($stmt2->fetch_assoc() == NULL) {
+            echo "<p>Actualmente no hay usuarios bloqueados</p>";
+        }
+
+        else {
+
+            echo("<td><b>Nombre de usuario</b></td>");
+            echo("<td><b>Correo electrónico</b</td>");
+            echo("<td><b>Cuenta verificada</b</td>");
+            echo("<td><b>Numero de inicios de sesion</b</td>");
+            echo("<td><b>Ultimo inicio de sesión</b</td>");
+            echo("<td><b>Administrar usuario</b</td>");
+        }
+        
+
+        while ( $row = $stmt2->fetch_assoc() ) {
+            echo "<tr><td>";
+            echo(htmlentities($row['username']));
+            echo "</td><td>";
+            echo(htmlentities($row['email']));
+            echo "</td><td>";
+            echo(htmlentities($row['account_verified'])? "Sí" : "No");
+            echo "</td><td>";
+            echo(htmlentities($row['num_of_logs']));
+            echo "</td><td>";
+            echo(htmlentities($row['last_log'])? htmlentities($row['last_log']) : "Sin registro");
+            echo "</td><td>";
+            echo(
+            '<form action="manage_user.php" method="post">
+            <input type="hidden" name="todo" value=unblock>
+            <input type="hidden" name="foo" value='.htmlentities($row["username"]).'>
+            <input type="submit" value="Desbloquear">
+            </form>'
+            );
+            echo("\n"."</td></tr>"."\n");
+        }
+    ?>
+    </table>
+
+
+<a href='admin.php'>Volver</a>
+
+
+
 </body>
 </html>
