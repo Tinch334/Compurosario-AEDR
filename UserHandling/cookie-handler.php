171252cbde329ffe9aa89e9282fe768ce7a642b0
maph <juani.bertoni73@gmail.com>
2022-10-14 21:34:06 -0300

    admin auth

--- UserHandling/cookie-handler.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "data.php";
-
-    //We check that we got the username and password
-    if(isset($_COOKIE["name"])) {
-        echo "Welcome " . $_COOKIE["name"] . "<br />";
-    }
-?>
--- scripts/csi.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    var includes = $('[data-include]')
-    $.each(includes, function () {
-        var file = $(this).data('include')
-      $(this).load(file)
-    })
-});
-
-//To use: <div data-include="<path-to-file>.html"></div>
