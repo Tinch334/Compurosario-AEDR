10e2d566f5e60a19576dd6aa0432a721d8ca9b53
Santi405 <zaninsantino@gmail.com>
2022-10-21 01:43:07 -0300

    omg Favincon workin 🤣

--- UserHandling/verify-email.php
+++ UserHandling/verify-email.php
@@ -23,7 +23,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link rel="icon" type="image/x-icon" href="Images/Main page/Favicon.png">
+        <link rel="icon" type="image/x-icon" href="Images/Main page/Favicon.ico">
     </head>
     <body>
         <?php
